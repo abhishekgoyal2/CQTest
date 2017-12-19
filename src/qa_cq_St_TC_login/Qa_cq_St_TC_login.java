9c2b454de44a36f9f0d903c951223e644aacfc8a
abhishek.goyal2 <abhi25goel@gmail.com>
2017-12-19 17:38:56 +0530

    commit 1 change

--- src/qa_cq_St_TC_login/Qa_cq_St_TC_login.java
+++ src/qa_cq_St_TC_login/Qa_cq_St_TC_login.java
@@ -4,6 +4,7 @@
 package qa_cq_St_TC_login;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import qa_cq_strack.Qa_ST_home;
@@ -14,32 +15,37 @@ import qa_cq_strack.Qa_ST_home;
  */
 @SuppressWarnings("unused")
 public class Qa_cq_St_TC_login  {
-static WebDriver  driver ;
+ WebDriver  driver ;
 	@Test
 	public void verifyvalidlogin() throws InterruptedException
 	{
 		System.setProperty("webdriver.gecko.driver", "C:/Projects/geckodriver-v0.16.0-win64/geckodriver.exe");
 
 
-		Qa_ST_home qa_Stlogin =new Qa_ST_home(driver);
+//		Qa_ST_home qa_Stlogin =new Qa_ST_home(driver);
+		driver =new FirefoxDriver();
+		
+		driver.manage().window().maximize();
 
-		Qa_ST_home.setupBrowser();
-		qa_Stlogin.typeusername("abhishek.goyal2@globallogic.com");
-		Thread.sleep(1000);
-		qa_Stlogin.typepassword("Welcome@01");
-		Thread.sleep(1000);
-		qa_Stlogin.clickOnLoginButton();
+ String  Appurl ="http://cdev-st01.cq.com:8080/watershed/login?";
+			driver.get(Appurl);
+//		Qa_ST_home.setupBrowser();
+//		qa_Stlogin.typeusername("abhishek.goyal2@globallogic.com");
+//		Thread.sleep(1000);
+//		qa_Stlogin.typepassword("Welcome@01");
+//		Thread.sleep(1000);
+//		qa_Stlogin.clickOnLoginButton();
 		
 //		Thread.sleep(5000);
 	
 //		qa_Stlogin.Statemapclickonhomepage();
 		
-		Thread.sleep(3000);
-	qa_Stlogin.AdvancedSearch();
+//		Thread.sleep(3000);
+//	qa_Stlogin.AdvancedSearch();
 //		login.Alert();
 		
-		Thread.sleep(1000);
-		Qa_ST_home.CloseBrowser();
+//		Thread.sleep(1000);
+//		Qa_ST_home.CloseBrowser();
 		
 	
 	}
